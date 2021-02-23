6322cceb65d5eecf10e87f514515a1bb0808f4ae
Philipp Klose <me@thehippo.de>
2021-02-23 01:47:13 +0100

    always return fulltext

--- functions.go
+++ functions.go
@@ -14,7 +14,7 @@ func decode(in []byte, out interface{}) error {
 
 // Search for podcasts, authors or owners
 func (c *Client) Search(term string) ([]*Podcast, error) {
-	return c.SearchC(term, false, false, 0)
+	return c.SearchC(term, false, 0)
 }
 
 // SearchC for searching with more options than Search
@@ -22,8 +22,8 @@ func (c *Client) Search(term string) ([]*Podcast, error) {
 // - clean for non explicit feeds according to itunes:explicit
 // - fullBody to return the more then 100 characters in the descriptions
 // - max for the number of results, when set to 0 it uses the API default
-func (c *Client) SearchC(term string, clean bool, fullText bool, max uint) ([]*Podcast, error) {
-	url := fmt.Sprintf("search/byterm?q=\"%s\"%s%s%s", term, addClean(clean), addFullText(fullText), addMax(max))
+func (c *Client) SearchC(term string, clean bool, max uint) ([]*Podcast, error) {
+	url := fmt.Sprintf("search/byterm?q=\"%s\"&fulltext%s%s", term, addClean(clean), addMax(max))
 	res, err := c.request(url)
 	if err != nil {
 		return nil, err
@@ -58,19 +58,19 @@ func (c *Client) getPodcast(url string, notFound error) (*Podcast, error) {
 // PodcastByFeedURL returns general information about a podcast by its
 // feed URL
 func (c *Client) PodcastByFeedURL(url string) (*Podcast, error) {
-	u := fmt.Sprintf("podcasts/byfeedurl?url=%s", url)
+	u := fmt.Sprintf("podcasts/byfeedurl?url=%s&fulltext", url)
 	return c.getPodcast(u, errors.New("Could not find a podcast for that feed URL"))
 }
 
 // PodcastByFeedID returns general information about a podcast by its id
 func (c *Client) PodcastByFeedID(id uint) (*Podcast, error) {
-	url := fmt.Sprintf("podcasts/byfeedid?id=%d", id)
+	url := fmt.Sprintf("podcasts/byfeedid?id=%d&fulltext", id)
 	return c.getPodcast(url, errors.New("Could not find a podcast for that id"))
 }
 
 // PodcastByITunesID returns general information about a podcast by its
 // ITune id
 func (c *Client) PodcastByITunesID(id uint) (*Podcast, error) {
-	url := fmt.Sprintf("podcasts/byitunesid?id=%d", id)
+	url := fmt.Sprintf("podcasts/byitunesid?id=%d&fulltext", id)
 	return c.getPodcast(url, errors.New("Could not find a podcast for that iTunes id"))
 }
